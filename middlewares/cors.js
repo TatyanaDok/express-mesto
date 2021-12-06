a84f18dd7b32d8633c2b78ba9a92a63171fb6efc
Tatyana D <yana.dok.89@mail.ru>
2021-12-06 15:42:12 +0300

    added nojekyll

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ module.exports.corsConfig = {
   origin: [
     'https://omicron.megatron.student.nomoredomains.rocks',
     'http://omicron.megatron.student.nomoredomains.rocks',
-    'http://localhost:3000',
+    'http://localhost:3001',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
   preflightContinue: false,
